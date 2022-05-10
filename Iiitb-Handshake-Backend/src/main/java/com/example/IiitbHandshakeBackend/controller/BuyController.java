cf27c5f3be05210bc02d0922a85f643e26b28b78
N1kunj1998 <njkhakhkhar@gmail.com>
2022-05-10 09:18:18 +0530

    changes in application.properties

--- Iiitb-Handshake-Backend/src/main/java/com/example/IiitbHandshakeBackend/controller/BuyController.java
+++ Iiitb-Handshake-Backend/src/main/java/com/example/IiitbHandshakeBackend/controller/BuyController.java
@@ -5,7 +5,6 @@ import com.example.IiitbHandshakeBackend.service.SellService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.websocket.server.PathParam;
 
 @CrossOrigin("*")
